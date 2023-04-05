841213c1f5eda27c5aa837bf488810067e48a792
adrianboh@gmail.com <adrianboh@gmail.com>
2023-04-05 11:24:43 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public Pizza(string nome, string descrizione, string foto, double prezzo)
+        {
+            Nome = nome;
+            Descrizione = descrizione;
+            Foto = foto;
+            Prezzo = prezzo;
+        }
+
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Foto { get; set; }
+        public double Prezzo { get; set; }
+
+    }
+}
